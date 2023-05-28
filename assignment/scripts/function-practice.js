22c8c069c8d46cb78f7ef52c8ce25be1a945f409
Amy Smith <smithniamyfse@gmail.com>
2023-05-28 14:51:35 -0500

    Answered questions 4-6 in function-practice.js

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -28,18 +28,20 @@ function addNumbers(firstNumber, secondNumber) {
 console.log(addNumbers(7, 3));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
-
+function multiplyThree(num1, num2, num3) {
+  return num1 * num2 * num3;
 }
-
+console.log(multiplyThree(2, 3, 4));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+  else {
+    return false;
+  }
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
@@ -47,15 +49,21 @@ console.log('isPositive - should say true', isPositive(3));
 console.log('isPositive - should say false', isPositive(0));
 console.log('isPositive - should say false', isPositive(-3));
 
-
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
-
+  if (array.length > 0) {
+    return array[array.length - 1]; 
+  }
+  else {
+    return undefined;
+  }
 }
+console.log('The last item of getLast is:', getLast(['Dana Scully', 'Fox Mulder', 'Walter Skinner', 'Cigarette Smoking Man']));
+console.log('The last item of getLast is:', getLast([]));
 
 // 7. Function to find a value in an array. Return true if the 
-//    value is found and false otherwise. Use a loop;
+//    vale is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
 
